82e71ac31805809dc49351d231db27e5610c39a7
Sushruth Nagaraj <74103829+sushnag22@users.noreply.github.com>
2023-04-26 12:50:31 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, send_file
 import io, json
 from PIL import Image
 from roboflow import Roboflow
